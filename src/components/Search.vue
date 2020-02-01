bbba41e3ed45176d6826786beaf9699f819cc4c2
Max <assassins08051998@gmail.com>
2020-02-01 12:36:56 +0200

    almoust done

---
+++ src/components/Search.vue
@@ -0,0 +1,33 @@
+<template>
+    <form class="search">
+        <input 
+            type="text"
+            :value="moviTitle"
+            @keyup="handleChange"
+        />
+        <input @click="handleSubmit" type="submit" value="SEARCH">
+    </form>
+</template>
+
+<script>
+import {ref} from '@vue/composition-api';
+
+export default {
+   name: 'Search',
+   props: ['search'],
+   setup ({search}, {emit}){
+       const moviTitle = ref(search);
+
+       return {
+           moviTitle,
+           handleSubmit(event) {
+               event.preventDefault();
+               emit('search', moviTitle.value)
+           },
+           handleChange(event) {
+               moviTitle.value = event.target.value
+           }
+       }
+   } 
+};
+</script>
